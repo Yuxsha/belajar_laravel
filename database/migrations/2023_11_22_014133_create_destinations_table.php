81257d81c4d07c5f2d93accea27e66a984385338
Yuxsha <azraff1982@gmail.com>
2023-11-22 08:54:58 +0700

    second commit

---
+++ database/migrations/2023_11_22_014133_create_destinations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->String("name",255);
+            $table->String("location",255);
+            $table->longtext("details");
+            $table->String("day-open",100);
+            $table->String("time-open",100);
+            $table->String("price",10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
